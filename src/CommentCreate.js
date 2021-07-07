0b499029a2f2dd5041e323abb794ad71814952dc
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-07 16:53:59 +0530

    Comment List

--- src/CommentCreate.js
+++ src/CommentCreate.js
@@ -1,20 +1,18 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
+const CommentCreate = ({ postId }) => {
+    const [content, setContent] = useState("");
 
+    const onSubmit = async (event) => {
+        event.preventDefault();
 
-export default ({ postId }) => {
-    const [content, setContent] = useState('')
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content,
+        });
 
-    const onSubmit = async (e) => {
-        e.preventDefault()
-
-        await axios.post(`http://localhost:4001/${postId}/comments`, {
-            content
-        })
-
-        setContent('')
-    }
+        setContent("");
+    };
 
     return (
         <div>
@@ -22,14 +20,15 @@ export default ({ postId }) => {
                 <div className="form-group">
                     <label>New Comment</label>
                     <input
-                        className="form-control"
                         value={content}
-                        onChange={e => setContent(e.target.value)}
+                        onChange={(e) => setContent(e.target.value)}
+                        className="form-control"
                     />
                 </div>
-                <button className="btn btn-primary">Post</button>
+                <button className="btn btn-primary">Submit</button>
             </form>
         </div>
-    )
+    );
+};
 
-}
+export default CommentCreate;
--- src/PostList.js
+++ src/PostList.js
@@ -1,34 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CommentCreate from './PostCreate'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
-    const [posts, setPosts] = useState({})
+    const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts')
-        setPosts(res.data)
-    }
+        const res = await axios.get("http://localhost:4000/posts");
+
+        setPosts(res.data);
+    };
 
     useEffect(() => {
-        fetchPosts()
-    }, [])
+        fetchPosts();
+    }, []);
 
-    const renderedPosts = Object.values(posts).map(post => {
-        return <div
-            className="card"
-            style={{ width: '30%', marginBottom: '20px' }}
-            key={post.id}
-        >
-            <div className="card-body">
-                <h3>{post.title}</h3>
-                <CommentCreate postId={post.id} />
+    const renderedPosts = Object.values(posts).map((post) => {
+        return (
+            <div
+                className="card"
+                style={{ width: "30%", marginBottom: "20px" }}
+                key={post.id}
+            >
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                    <CommentList postId={post.id} />
+                    <CommentCreate postId={post.id} />
+                </div>
             </div>
-        </div>
-    })
-    return <div className="d-flex flex-row flex-wrap justify-content-between">
-        {renderedPosts}
-    </div>
-}
+        );
+    });
 
-export default PostList
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {renderedPosts}
+        </div>
+    );
+};
+
+export default PostList;
